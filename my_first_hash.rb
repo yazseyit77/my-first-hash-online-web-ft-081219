fa98118f606ec83fb720b2d9672a985d6f7cef1f
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-18 15:01:11 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -21,7 +21,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+  return shipping_manifest["oil paintings"]
 end
 
 def adding
